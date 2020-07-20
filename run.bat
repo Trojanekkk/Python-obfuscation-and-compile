9c015b7c1a6cd8229bfe2c1e1d03ae2b5f4ad434
Wojciech Trojanowski <W.Trojan27@gmail.com>
2020-07-20 18:13:32 +0200

    Add: main.py structure

--- run.bat
+++ run.bat
@@ -1,6 +1,6 @@
 @ECHO off
 ECHO [97;100m Welcome to "Protect Python code and expose as exe file" [0m
-
+ECHO.
 ECHO [97;100m Installing cython and pyinstaller [0m
 REM pip install pyinstaller
 REM pip install cython
@@ -28,8 +28,19 @@ ECHO setup(>>                                       .\__result__.\compile.py
 ECHO     ext_modules=cythonize('%module_name%')>>   .\__result__.\compile.py
 ECHO )>>                                            .\__result__.\compile.py
 
-findstr import .\__result__\%module_name%>>         .\__result__.\main.py
+ECHO [97;100m Set name of python class or function you want to import to main [0m
+SET /p export_name="Type <export logic name>: 
 
-REM python .\compile.py build_ext --inplace
+ECHO # Modify this file to be albe run Your appliaction from the file.>>    .\__result__.\main.py
+findstr import .\__result__\%module_name%>>                                 .\__result__.\main.py
+ECHO from %module_name% import %export_name%>>                              .\__result__.\main.py
+ECHO if __name__ == "__main__":>>                                           .\__result__.\main.py
+ECHO     app = %export_name%()>>                                            .\__result__.\main.py
+
+ECHO [101;93m Modify .\__result__\main.py, to be able run application from main.py file and continue this script [0m
+PAUSE
+
+[97;100m Building app [0m
+python .\__result__\compile.py build_ext --inplace
 REM pyinstaller .\main.py
 PAUSE
